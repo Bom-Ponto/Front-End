e701f7566b6a1d1367667049d56c9c26693562c4
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-11-16 13:45:12 -0300

    feat: add products register page script

---
+++ Scripts/products-register-page.js
@@ -0,0 +1,59 @@
+const btnSaveProduct = document.getElementById('btnSalvarProduto');
+const inputProdName = document.getElementById('nomeProduto');
+const inputProdPrice = document.getElementById('valorProduto');
+const inputProdBarCode = document.getElementById('codigoProduto');
+const inputProdValidUnitl = document.getElementById('validade');
+const inputProdImgUrl = document.getElementById('urlImg');
+
+function register() {
+  if (
+    !inputProdName.value ||
+    !inputProdPrice.value ||
+    !inputProdBarCode.value ||
+    !inputProdValidUnitl.value
+  ) {
+    return Swal.fire({
+      title: 'Oops!',
+      text: 'Parece que você não preencheu todos os campos',
+      icon: 'error',
+      confirmButtonText: 'OK',
+    });
+  }
+
+  const URL = 'https://bomponto.herokuapp.com/api/products';
+  const requestOpt = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: inputProdName.value,
+      price: inputProdPrice.value,
+      barCode: inputProdBarCode.value,
+      validUntil: inputProdValidUnitl.value,
+      pictureServerPath: inputProdImgUrl.value,
+    }),
+    mode: 'cors',
+  };
+
+  fetch(URL, requestOpt)
+    .then(() => {
+      Swal.fire({
+        title: 'Pronto!',
+        text: 'Produto cadastrado.',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      });
+    })
+    .catch(() => {
+      Swal.fire({
+        title: 'Oops!',
+        text: 'Algo deu errado...',
+        icon: 'error',
+        confirmButtonText: 'OK',
+      });
+    });
+}
+
+btnSaveProduct.addEventListener('click', register);
